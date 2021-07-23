35cd583c2790b6376f374325733db75f79cb82fd
Michael Hannalla <michael.sami.hanna@gmail.com>
2021-07-24 01:27:59 +0200

    Finalized required code

--- feature_matching.py
+++ feature_matching.py
@@ -1,52 +1,61 @@
 import cv2
 import numpy as np
 import os
+import xlsxwriter
 from PIL import Image
 
-good_matches_threshold = 10
+def main():
+    #TODO: using args
+    
+    good_matches_threshold = 40         # Threshold for matches between images to be considered same item
 
-folder1_path = "/home/heidi/Feature Matching Project/sample"
-folder2_path = "/home/heidi/Feature Matching Project/sample2"
+    samples_folder = "sample1"          # Path for the samples folder
+    database_folder = "sample2"         # Path for the database folder
 
-#img1 = cv2.imread('sample/a01000.jpg')
-#img2 = cv2.imread('sample2/a01010.jpg')
+    correspondences = []                # Empty list to hold correspondences
 
+    samples_list = os.listdir(samples_folder)       # Get everything in the samples folder directory
+    database_list = os.listdir(database_folder)     # Get everything in the database folder directory
 
-img_1 = os.listdir(folder1_path)
-img_2 = os.listdir(folder2_path)
+    orb = cv2.ORB_create(nfeatures= 1000)           # Create the orb feature extractor
 
-#loop on every image in both folders
-for img_i_path in img_1:
-    for img_j_path in img_2:
-        #read everyimage in folder 1 and folder 2
-        img1 = Image.open(folder1_path + "/" + img_i_path)
-        img2 = Image.open(folder1_path + "/" + img_j_path)
+    # Loop on every combination of images in both folders
+    for idx1, sample_img_path in enumerate(samples_list):
+        
+        # Open the sample image, then convert to openCV type, then change color space.
+        sample_img = Image.open(samples_folder + "/" + sample_img_path)
+        sample_img = np.array(sample_img)   # convert from PIL image to openCV
+        sample_img = cv2.cvtColor(sample_img, cv2.COLOR_BGR2RGB) 
 
-        # convert from PIL image to openCV 
-        open_cv_image_1 = np.array(img1) 
-        open_cv_image_2 = np.array(img2) 
-        img1 = cv2.cvtColor(open_cv_image_1, cv2.COLOR_BGR2RGB) 
-        img2 = cv2.cvtColor(open_cv_image_2, cv2.COLOR_BGR2RGB) 
+        # Loop on each image in the database
+        for idx2, database_img_path in enumerate(database_list):
 
-        orb = cv2.ORB_create(nfeatures= 1000)
+            # Read every image in samples folder and database folder, do same conversions on sample image
+            database_img = Image.open(database_folder + "/" + database_img_path)
+            database_img = np.array(database_img)    # convert from PIL image to openCV
+            database_img = cv2.cvtColor(database_img, cv2.COLOR_BGR2RGB) 
 
-        kp1, des1 = orb.detectAndCompute(img1, None)
-        kp2, des2 = orb.detectAndCompute(img2, None)
+            kp1, des1 = orb.detectAndCompute(sample_img, None)          # detect keypoints and their descriptors
+            kp2, des2 = orb.detectAndCompute(database_img, None)
 
-        bf = cv2.BFMatcher()
-        matches = bf.knnMatch(des1, des2, k=2)
+            bf = cv2.BFMatcher()                                        # create the brute-force matcher
+            matches = bf.knnMatch(des1, des2, k=2)                      # perform knn matching with 2 nearest neighbours
 
-        good_matches = []
-        for m,n in matches:
-            if m.distance < 0.75*n.distance:
-                good_matches.append([m])
+            good_matches = []                                           # list of matches
+            for m,n in matches:
+                if m.distance < 0.75*n.distance:
+                    good_matches.append([m])    
 
-                if len(good_matches) > good_matches_threshold:
-                    print(img_i_path, img_j_path)
+            if len(good_matches) > good_matches_threshold:
+                print("Match found between {} in samples folder and {} in database folder".format(sample_img_path, database_img_path))
+                correspondences.append([sample_img_path, database_img_path])
+                break                                                   # break and stop searching for this if a match is found (this to allow faster search)
 
-        matches_img = cv2.drawMatchesKnn(img1, kp1, img2, kp2, good_matches, None, flags=2)
+    with xlsxwriter.Workbook('excels/correspondences.xlsx') as workbook:         # excel writing session
+        worksheet = workbook.add_worksheet()
 
+        for row_num, data in enumerate(correspondences):
+            worksheet.write_row(row_num, 0, data)
 
-        matches_img = cv2.resize(matches_img, (matches_img.shape[1]//5, matches_img.shape[0]//5))
-        #cv2.imshow('matches', matches_img)
-        #cv2.waitKey(0)
+if __name__ == "__main__":
+    main()
