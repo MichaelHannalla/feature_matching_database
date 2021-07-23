4adf8ad3433eeeebdab0da27185026bed16754e8
Michael Hannalla <michael.sami.hanna@gmail.com>
2021-07-23 17:48:37 +0200

    Added  template matching file

---
+++ template_matching.py
@@ -0,0 +1,44 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+def main():
+
+    img = cv2.imread("sample_data/a01000.jpg")
+    img = cv2.resize(img, (img.shape[1]//3, img.shape[0]//3))
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img2 = img.copy()
+    template = cv2.imread("templates/pic1.jpg")
+    template = cv2.cvtColor(template, cv2.COLOR_BGR2RGB)
+    w, h = template.shape[1], template.shape[0]
+    
+    # All the 6 methods for comparison in a list
+    methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR',
+                'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
+    
+    for meth in methods:
+        img = img2.copy()
+        method = eval(meth)
+        # Apply template Matching
+        res = cv2.matchTemplate(img,template,method)
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+
+        # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
+        if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
+            top_left = min_loc
+        else:
+            top_left = max_loc
+        
+        bottom_right = (top_left[0] + w, top_left[1] + h)
+        
+        cv2.rectangle(img,top_left, bottom_right, 255, 2)
+        plt.subplot(121),plt.imshow(res,cmap = 'gray')
+        plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
+        plt.subplot(122),plt.imshow(img,cmap = 'gray')
+        plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
+        plt.suptitle(meth)
+        plt.show()
+
+
+if __name__ == "__main__":
+    main()
