1c1f23e3e9118b67695e0037e274ad064e1a73b8
Michael Hannalla <michael.sami.hanna@gmail.com>
2021-07-30 11:56:52 +0200

    Added second chance feature

--- feature_matching_rapid.py
+++ feature_matching_rapid.py
@@ -8,14 +8,17 @@ from utils import check_color_feaasibility, load_descriptors, read_image
 from tqdm import tqdm
 
 def main():
+
     #TODO: using args
     
-    good_matches_threshold = 60         # Threshold for matches between images to be considered same item
-    use_bruteforce = True               # Choosing wheter to use the brute force matcher or flann matcher
-    one_by_one = True                   # Choosing whether we'll compare each two combinations of images and get the best not just stop by the first good match
+    good_matches_threshold = 60             # Threshold for matches between images to be considered same item
+    use_bruteforce = True                   # Choosing wheter to use the brute force matcher or flann matcher
+    one_by_one = True                       # Choosing whether we'll compare each two combinations of images and get the best not just stop by the first good match
+    give_second_chance = True               # Choosing whether we'll give a chance to low ratio matches with one image in database
+    second_chance_match_threshold = 45      # Threshold for low ratio matches
 
-    samples_folder = "sample1"          # Path for the samples folder
-    database_folder = "sample2"         # Path for the database folder
+    samples_folder = "sample1"              # Path for the samples folder
+    database_folder = "sample2"             # Path for the database folder
     
     correspondences = [['sample', 'database', 'match score']]                # Empty list to hold correspondences, will later be converted to an excel file
 
@@ -35,8 +38,12 @@ def main():
     print("Finished loading all the database descriptors")
     
     for sample_idx, sample_img_path in enumerate(tqdm(samples_list)):
+        
+        if give_second_chance:
+            hit_count = 0
 
         curr_max_match_number = 0                                                             # Initialize a variable that holds the max matches of all
+        max_match_number_unguaranteed = 0
 
         print("_Comparing sample {}/{}.".format(sample_idx, len(samples_list)))
         # Open the sample image, then convert to openCV type, then change color space.
@@ -47,7 +54,7 @@ def main():
             database_des, database_name, database_img = database_point
 
             if use_bruteforce:
-                matches = bf.knnMatch(sample_des, database_des, k=2)                          # perform knn matching with 2 nearest neighbours
+                matches = bf.knnMatch(sample_des, database_des, k=2)                            # perform knn matching with 2 nearest neighbours
             else:
                 pass
                 print("Feature unsupported at current version release")
@@ -56,13 +63,21 @@ def main():
                 #des2 = des2.astype(np.float32)
                 #matches = flann.knnMatch(des1, des2, k=2)
 
-            good_matches = []                                                   # list of matches
+            good_matches = []                                                                   # list of matches
             for m, n in matches:
                 if m.distance < 0.75*n.distance:
                     good_matches.append([m])    
+            
+            if (len(good_matches)) > max_match_number_unguaranteed:
+                max_match_number_unguaranteed = len(good_matches)
+                max_match_unguaranteed = database_name
+                
+            if (len(good_matches) > second_chance_match_threshold) and give_second_chance:
+                hit_count += 1
+                low_ratio_match = database_name
+                low_ratio_match_number = len(good_matches)
 
             if len(good_matches) > good_matches_threshold:
-
                 if not check_color_feaasibility(sample_img, database_img):
                     continue
 
@@ -80,9 +95,13 @@ def main():
             print("Match found between {} in samples folder and {} in database folder".format(sample_img_path, curr_max_match))
             correspondences.append([sample_img_path, curr_max_match, curr_max_match_number])
 
+        elif hit_count == 1 and give_second_chance:
+            print("Low ratio match found between {} in samples folder and {} in database folder".format(sample_img_path, low_ratio_match))
+            correspondences.append([sample_img_path, low_ratio_match, low_ratio_match_number, "LOW RATIO MATCH"])
+
         else:    
             print("No match found for sample {} in the database folder".format(sample_img_path))
-            correspondences.append([sample_img_path, "NO MATCH", curr_max_match_number, curr_max_match])            # Print also the most likely match even if not successful
+            correspondences.append([sample_img_path, "NO MATCH", max_match_number_unguaranteed, max_match_unguaranteed])            # Print also the most likely match even if not successful
 
     if use_bruteforce:
         excel_suffix = "bf"
